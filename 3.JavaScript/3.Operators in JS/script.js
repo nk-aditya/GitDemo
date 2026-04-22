4b11c2ead4a31290c6685c81873728077bed350c
nk-aditya <adityanayak2803@gmail.com>
2026-04-23 03:10:03 +0530

    add Operators in folder in JS folder and learn about JS operators

---
+++ 3.JavaScript/3.Operators in JS/script.js
@@ -0,0 +1,115 @@
+// // Assignment Operator (=)
+// const a=5;
+// const b=3;
+
+// //Addition operator (+)
+// const sum=a+b;
+// console.log(sum);
+
+// //Subtraction operator (-)
+// const sub=a-b;
+// console.log(sub);
+
+// //Multiplication operator (*)
+// const mul=a*b;
+// console.log(mul);
+
+// //Division operator (/)
+// const div=a/b;
+// console.log(div);
+
+// //Modulos operator (% for remainder)
+// const rem=a%b;
+// console.log(rem);
+
+// //Increment Operator (++)
+// let abc=12;
+// abc++;                   // basically it is shortform of abc = abc + 1;
+// console.log(abc);
+
+
+// //Decrement Operator (--)
+// let xyz=12;
+// xyz--;                   //similarlly it is short form of xyz=xyz - 1;
+// console.log(xyz);
+
+// //Post Increment 
+// let x=10;
+// console.log(x);
+// console.log(x++);    //increse after execution of the line basically in the next line
+// console.log(x)
+
+// //Pre Increment
+// let y=10;
+// console.log(y);
+// console.log(++y);  //increse in the same line 
+
+// // similary Post and Pre work sam for decrement also
+// //Post Decrement 
+// let g=10;
+// console.log(g);
+// console.log(g--);    //decrese after execution of the line basically in the next line
+// console.log(g)
+
+// //Pre Decrement
+// let h=10;
+// console.log(h);
+// console.log(--h);  //decrese in the same line
+
+// //Exponential Operator (**)
+// let a = 2;
+// let b = 3;
+// const result = a**b;
+// console.log(result);
+
+// //Shorthand operator
+// let a =3;
+// let b=2;
+// a+=b;
+// console.log(a);
+// console.log(b);
+
+//similarly there are some shorthand operators 
+// a = a+b;        a+=b;
+// a = a-b;        a-=b;
+// a = a*b;        a*=b;
+// a = a/b;        a/=b;
+// a = a%b;        a%=b;
+// a = a**b;       a**=b;
+
+// Comparison operators
+// Equals (==)
+// let a= 5;
+// let b= 3;
+// console.log(a==b);
+
+//Strict operator
+// let a = 5;
+// let b= "5";
+// console.log(a==b);  // only check value
+// console.log(a===b); //check data types also
+
+//similary for greather than(<) , less than(>) and greather than equall to(<=_) , lesss than equal to(>=).
+
+//Logical operator
+// //Logical And (&&)
+// const a=true;
+// const b=true;
+// const result = a && b;  // for AND true only come when both are true 
+// console.log(result);
+
+// //Logical Or (||)
+// const a=true;
+// const b=false;
+// const result = a || b;  // for OR true come if any one of the two are true 
+// console.log(result);
+
+
+// //Logical Not (!)
+// const a=false;
+// const result = ! a;  // NOT just inverse the value, if it is true it gonna be false, and if false vice-versa.
+// console.log(result);
+
+
+
+
