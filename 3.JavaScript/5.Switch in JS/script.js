b1d7d1e399ab1110681c213418a4117a6fdc1089
nk-aditya <adityanayak2803@gmail.com>
2026-04-23 04:00:12 +0530

    learn about switch operator

---
+++ 3.JavaScript/5.Switch in JS/script.js
@@ -0,0 +1,47 @@
+//Else-if Ladder
+// const score = 28;
+// if(score>=90){
+//     console.log("A++"); 
+// }else if(score>=80){
+//     console.log("A"); 
+// }else if(score>=70){
+//     console.log("B"); 
+// }else if(score>=60){
+//     console.log("C"); 
+// }else if(score>=45){
+//     console.log("D"); 
+// }else if(score<=45 && score>=30){
+//     console.log("E"); 
+// }else if(score<=29){
+//     console.log("F"); 
+// }
+
+// Switch Condition
+// const score=50;
+// switch(score){
+//     case 90:
+//         console.log("A++");
+//         break;
+//     case 80:
+//         console.log("A");
+//         break;
+//     case 70:
+//         console.log("B");
+//         break;
+//     case 60:
+//         console.log("C");
+//         break;
+//     case 50:
+//         console.log("D");
+//         break;
+//     case 40:
+//         console.log("E");
+//         break;
+//     case 30:
+//         console.log("F");
+//         break;
+//     default:
+//         console.log("need to improve");
+//         break;
+// }
+
