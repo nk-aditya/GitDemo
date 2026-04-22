01037b3e62b68100b61fa778c584525d1bd08656
nk-aditya <adityanayak2803@gmail.com>
2026-04-23 03:43:21 +0530

    add If-Else Condition folder in JS folder and learn about JS If-Else Condition

---
+++ 3.JavaScript/4.If-Else Condition/script.js
@@ -0,0 +1,60 @@
+// const candidateAge = 70;
+ 
+// //If Condition
+// if(candidateAge>=18){
+//     console.log("Hey you can vote");
+// }
+
+//If-Else Condition
+// if(candidateAge>=18){
+//     console.log("Hey you can vote");
+// }else{
+//     console.log("Hey you cannot vote");
+    
+// }
+
+//Nested if-else
+// if(candidateAge>=18){
+//     if(candidateAge>=60){
+//         console.log("Senior citizens are eligiblr to vote online ");
+        
+//     }
+//     console.log("Hey you can vote");
+// }else{
+//     console.log("Hey you cannot vote");
+    
+// }
+
+//If-Else Ladder
+// if(candidateAge>=60){
+//     console.log("Senior citizens are eligiblr to vote online ");
+// }else if(candidateAge>=18){
+//    console.log("Hey you can vote");
+// }
+// else{
+//     console.log("Hey you cannot vote");
+    
+// }
+
+// const score = 28;
+// if(score>=90){
+//     console.log("A++"); 
+// }else if(score>=80){
+//     console.log("A"); 
+// }else if(score>=70){
+//     console.log("B"); 
+// }else if(score>=60){
+//     console.log("C"); 
+// }else if(score>=45){
+//     console.log("D"); 
+// }else if(score<=45 && score>=30){
+//     console.log("E"); 
+// }else if(score<=29){
+//     console.log("F"); 
+// }
+
+
+// Ternary Operator
+// const candidateAge = 70;
+// candidateAge>18 ? console.log("Eligible to vote") : console.log("Not eligible to Vote");
+
