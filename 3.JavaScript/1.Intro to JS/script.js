fcb82623fe6203f97ea6680b104d4512cacccb92
nk-aditya <adityanayak2803@gmail.com>
2026-04-23 01:35:06 +0530

    add intro to js folder in js folder

---
+++ 3.JavaScript/1.Intro to JS/script.js
@@ -0,0 +1,36 @@
+//  // let,const,var
+//  let age="22";
+//  let name="aditya"; 
+//  let isEligibleToVote = true;
+//  console.log("my name is",name);
+//  console.log("i am",age,"years old");
+//  console.log(isEligibleToVote);
+ 
+//  let a=12;
+//  let b=10;
+//  let c=a+b;
+//  console.log(c);
+//  console.log(c-5);
+
+// let varName="aditya";
+// console.log(varName);
+// varName=28;
+// console.log(varName);
+
+// const office = "ABC";
+// console.log(office)
+// office="XYZ";
+// console.log(office);
+
+// var college="IIt";
+// college="NIT";
+// console.log(college);
+
+// let varName; //Declaration
+// let varName="ABC";  //Initialisation
+
+// let varName;  //Not Allowed to declared variable with same name
+// let VarName;  //Allowed because js is case-sensitive
+// let 123var;  //NotAllowed varaible name not starts with number
+// let var123;  //Allowed to use numbers for varaible name expect in starting of the name
+// let var;     //Keywords are not recomended to use as variable VarName
