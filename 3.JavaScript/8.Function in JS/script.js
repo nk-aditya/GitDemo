c500357020b6382d023c688a262513a6540e5fb8
nk-aditya <adityanayak2803@gmail.com>
2026-04-24 01:32:09 +0530

    learn about function in JS and some basic types of functipon in JS

---
+++ 3.JavaScript/8.Function in JS/script.js
@@ -0,0 +1,65 @@
+// //Code Snippet
+// const a=10;
+// const b=2;
+// // const aPlusB=a+b;
+// // console.log("Result:",aPlusB);
+// submition(a,b);
+
+// //Code Snippet
+// const x=12;
+// const y=6;
+// // const xPlusY=x+y;
+// // console.log("Result:",xPlusY);
+// submition(x,y);
+
+// //Code Snippet
+// const l =20;
+// const k=4;
+// // const lPlusK=l+k;
+// // console.log("Result:",lPlusK);
+// submition(l,k);
+
+// //Function declaration
+// function submition(r,t){
+//     const result=r+t;
+//     console.log("Result:",result);
+// }
+
+// //function calling
+// submition(10,20);      
+
+// // function greet(){
+// //     console.log("hey welcome to our website");
+// // }
+// // greet();
+
+// // function greet(userName){
+// //     console.log("hey welcome to our website",userName);
+// // }
+// // greet("Aditya");
+
+//Types of Function 
+// //Named Function
+// function submition(r,t){                 //function with function userName also hosted in top
+//     const result=r+t;
+//     console.log("Result:",result);
+// }
+
+//Anonymous function / func expression
+// let submition = function (r,t){           //write function without function name and storee in a variable also it is not hosted
+//     const result=r+t;
+//     return result;
+// }
+// // anonymous function call
+// console.log(submition(20,30));
+ 
+// //Nested Function
+// function addSquare(x,y){            //Outer function
+//     function square(num){           //Inner function , only loacl scope of outer function
+//        return num*num; 
+//     }
+//     console.log(square(4));
+// } 
+
+
+// addSquare(1,2);
