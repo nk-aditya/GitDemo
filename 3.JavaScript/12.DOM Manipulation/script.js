eb4a542c69636c93f24759453c96238fa6338784
nk-aditya <adityanayak2803@gmail.com>
2026-04-26 19:07:26 +0530

    understanding DOM Manipulation

---
+++ 3.JavaScript/12.DOM Manipulation/script.js
@@ -0,0 +1,49 @@
+// console.log(document.body);
+// const bodyBackground = document.body;
+// bodyBackground.style.backgroundColor = "green";
+
+//with id name
+// const box4=document.getElementById("box-4");
+// box4.style.color= "white";
+
+//with tag name
+// const h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+
+//with class name
+// const boxes=document.getElementsByClassName("box");
+// console.log(boxes);
+
+//modifying html element
+// const box1=document.getElementById('box-1');
+// // box1.innerHTML="<h2>Aditya</h2>"; 
+// box1.innerText="aditya";
+
+// document.getElementsByTagName('p');
+
+// const box2=document.getElementById('box-2');
+// box2.classList.add('circle');
+
+// const box3=document.getElementById('box-3');
+// // box3.classList.remove('circle');
+
+// const boxes=document.getElementsByClassName('box');
+// for (let i =0;i<boxes.length;i++){
+//     boxes[i].classList.add('circle');
+// }
+
+// box3.classList.remove('box');
+
+//creating a new element
+//1st to get the acces
+//2nd is to content
+//3rd is appending
+
+// const newPara = document.createElement('p');
+// newPara.innerText='This is our new paragraph';
+
+// const cont=document.getElementById('cont');
+// cont.appendChild(newPara);
+
+// newPara.classList.add('box');
+// newPara.classList.add('circle');
